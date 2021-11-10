26cf2994fbc896ad5e1472d74d08131c7bb2753b
Chris Cassano <glitch0@gmail.com>
2021-11-09 19:40:46 -0800

    update metadata creation

--- createMetadataJsonFiles.js
+++ createMetadataJsonFiles.js
@@ -1,10 +1,13 @@
-const Arweave = require('arweave');
-const fs = require('fs');
-const dagPB = require('ipld-dag-pb');
-const { UnixFS } = require('ipfs-unixfs');
+const Arweave = require("arweave");
+const fs = require("fs");
+const dagPB = require("ipld-dag-pb");
+const { UnixFS } = require("ipfs-unixfs");
 
-
-async function getIpfsCid(data, unixFsType = 'file', dagPbOptions = { cidVersion: 0 }) {
+async function getIpfsCid(
+  data,
+  unixFsType = "file",
+  dagPbOptions = { cidVersion: 0 }
+) {
   const file = new UnixFS(unixFsType, data);
 
   const node = new dagPB.DAGNode(file.marshal());
@@ -14,64 +17,8 @@ async function getIpfsCid(data, unixFsType = 'file', dagPbOptions = { cidVersion
   return Cid.toBaseEncodedString();
 }
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function shuffle(array) {
-  var currentIndex = array.length, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-}
-
-
-function generateDescription(word) {
-  if (!word.definitions) {
-    throw new Error(`${word.word} is missing a definition`)
-  }
-
-  let desc = ''
-  if (word.definitions.length === 1) {
-    const d = word.definitions[0]
-    desc += `${d.partOfSpeech} - `
-    if (d.definitions.length === 1) {
-      desc += d.definitions[0].definition
-    } else {
-      d.definitions.forEach((def, j) => {
-        desc += `${String.fromCharCode(97 + j)}) ${def.definition} `
-        // if (def.example) {
-        //   desc += `\n  // ${def.example}`
-        // }
-      })
-    }
-  } else {
-    word.definitions.forEach((d, i) => {
-      desc += `${i + 1}: ${d.partOfSpeech} - `
-      d.definitions.forEach((def, j) => {
-        desc += `${String.fromCharCode(97 + j)}) ${def.definition} `
-        // if (def.example) {
-        //   desc += `\n  // ${def.example}`
-        // }
-      })
-    })
-  }
-  return desc
-}
-
 async function loadKeyFromFile(path) {
-  let data = '';
+  let data = "";
 
   try {
     data = fs.readFileSync(path);
@@ -86,41 +33,49 @@ async function loadKeyFromFile(path) {
   try {
     const decoded = JSON.parse(data);
 
-    if (typeof decoded !== 'object') {
-      throw new Error('Failed to parse Arweave key file, the file format is invalid');
+    if (typeof decoded !== "object") {
+      throw new Error(
+        "Failed to parse Arweave key file, the file format is invalid"
+      );
     }
 
     return decoded;
   } catch (error) {
-    throw new Error('Failed to parse Arweave key file, the file format is invalid');
+    throw new Error(
+      "Failed to parse Arweave key file, the file format is invalid"
+    );
   }
 }
 
 async function main() {
-  const allTxns = []
+  const allTxns = [];
 
   const arweave = Arweave.init({
-    host: 'arweave.net',
+    host: "arweave.net",
     port: 443,
-    protocol: 'https'
+    protocol: "https",
   });
-  const key = await loadKeyFromFile('/Users/chris/.arweave/arweave-keyfile-04FYC9p-s_IDTFh51eYhbJ7GgpN7RcxGuy053TjhmTE.json')
+  const key = await loadKeyFromFile(
+    "/Users/chris/.arweave/arweave-keyfile-04FYC9p-s_IDTFh51eYhbJ7GgpN7RcxGuy053TjhmTE.json"
+  );
 
-  let words = shuffle(JSON.parse(fs.readFileSync(`./words.json`)));
+  let images = shuffle(JSON.parse(fs.readFileSync(`./arweaveImages.json`)));
 
-  for (let i = 0; i < words.length; i++) {
-    const word = words[i]
+  for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+    const tokenId = i + 1;
 
-    console.log(`processing ${i + 1} of ${words.length}: ${word.word}`)
+    console.log(`processing ${i + 1} of ${images.length}: ${image.filename}`);
 
     const metadata = {
-      name: capitalizeFirstLetter(word.word),
-      description: generateDescription(word),
-      image: `https://arweave.net/${word.id}`
-    }
+      name: "Lit Genesis Gate",
+      description:
+        "To the owner of this NFT, congrats. You possess the key to a special portal. Behind this gate you'll find collaborative art, access to a private channel in the Lit Protocol discord, and other features and benefits for NFT owners.  Click on the NFT to enter!",
+      image: `https://arweave.net/${image.id}`,
+      animation_url: `https://litgateway.com/LitOgNft.html?tokenId=${tokenId}`,
+    };
 
-    fs.writeFileSync(`./final/${i + 1}.json`, JSON.stringify(metadata))
-
+    fs.writeFileSync(`./final/${tokenId}.json`, JSON.stringify(metadata));
 
     // let data = fs.readFileSync(`${path}/${file}`);
 
@@ -139,7 +94,6 @@ async function main() {
     //   console.log(`${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`);
     // }
 
-
     // console.log(transaction)
 
     // allTxns.push({
@@ -148,14 +102,13 @@ async function main() {
     // })
   }
 
-  console.log('------------------------------')
-  console.log('done!')
-  console.log('------------------------------')
+  console.log("------------------------------");
+  console.log("done!");
+  console.log("------------------------------");
 
-  return allTxns
+  return allTxns;
 }
 
-
 main().then((allTxns) => {
-  console.log(JSON.stringify(allTxns))
-})
+  console.log(JSON.stringify(allTxns));
+});
